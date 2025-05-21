e0ecc677ecccd3ca76243d9b277ba001e66361c5
Patrockanite <patrickannoot@orange.fr>
2025-05-21 19:42:27 +0200

    modif code Equation_second_degre.py

--- Scripts_Cahpitre_3/Equation_second_degre.py
+++ Scripts_Cahpitre_3/Equation_second_degre.py
@@ -13,4 +13,23 @@
     x = -b/2a
 
 Si la valeur de delta est strictement négative, l’équation n’a pas de racine réelle.
+méthode : il faut calculer le discriminant : Delta = b^2-4ac
+Donc pour -x^2+x+6=0 ==> a=-1 , b=1 et c=6 ==> -1*x^2 + 1*x + 6 = 0
 """
+import math
+
+print("Résolution d'une équation de second degré ax^2 + bx + c = 0")
+a = float(input("Pour ax^2, valeur de a = "))
+b = float(input("Pour bx, valeur de b = "))
+c = float(input("Valeur de c = "))
+# Calcul du discriminant
+delta = b**2-4*a*c
+print("Discriminant delta =",delta)
+if delta < 0:
+    print("Il n'y a pas de solution dans R")
+elif delta == 0:
+    print("Il n'existe qu'une solution : x =",-1*b/(2*a))
+else:
+    print("Il existe 2 solutions à l'équation :")
+    print("x1 =",(-1*b-math.sqrt(delta))/(2*a))
+    print("x2 =",(-1*b+math.sqrt(delta))/(2*a))
